860e69e0e76cf335c948c790793740ab05c93f22
ramachandran <93141917+chandran-2003-ram@users.noreply.github.com>
2023-07-13 07:18:23 +0530

    Update reducer.js

--- src/components/Books/reducer.js
+++ src/components/Books/reducer.js
@@ -1,31 +1,3 @@
-
-import book1 from "../images/apj.jpg";
-import book2 from "../images/city-under-one-roof-book.png";
-import book3 from "../images/the-world-and-all-that-it-holds-book.png";
-import book4 from "../images/maame-book.png";
-import book5 from "../images/essex-dogs-book.png";
-import book6 from "../images/what-happened-to-ruthy-ramirez-book.png";
-import book7 from "../images/a-death-at-the-party-book.png";
-import book8 from "../images/the-god-of-endings-book.png";
-import book9 from "../images/now-you-see-us-book.png";
-import book10 from "../images/dust-child-book.png";
-import book11 from "../images/the-mostly-true-story-of-tanner-and-louise-book.png";
-import book12 from "../images/homecoming-book.png";
-import book13 from "../images/symphony-of-secrets-book.png";
-import book14 from "../images/silver-alert-book.png";
-import book15 from "../images/only-love-can-hurt-like-this-book.png";
-import book16 from "../images/paper-names-book.png";
-import book17 from "../images/the-collected-regrets-of-clover-book.png";
-import book18 from "../images/yellowface-book.png";
-import book19 from "../images/the-second-ending-book.png";
-import book20 from "../images/the-wishing-game-book.png";
-import book21 from "../images/same-time-next-summer-book.png";
-import book22 from "../images/banyan-moon-book.png";
-import book23 from "../images/crook-manifesto-book.png";
-import book24 from "../images/everyone-here-is-lying-book.png";
-import book25 from "../images/gone-tonight-book.png";
-
-
 export  var initialState = {
 
   cartList:[],
@@ -34,7 +6,7 @@ export  var initialState = {
     price: '1.02',
     rating: '4',
     quantity: '5',
-    image: book3,
+    image: 'http://prodimage.images-bn.com/pimages/9780593544549_p0_v3_s1200x630.jpg',
  
 
   },
@@ -43,56 +15,56 @@ export  var initialState = {
     price: '2.03',
     rating: '3',
     quantity: '5',
-    image: book7,
+    image: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781668009109/a-death-at-the-party-9781668009109_lg.jpg',
   },
   {
     title: 'The Banyan Moon Book',
     price: '3.04',
     rating: '2',
     quantity: '5',
-    image: ,
+    image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1675641427i/62800971._UY630_SR1200,630_.jpg',
   },
   {
     title: 'WINGS OF FIRE',
     price: '4.00',
     rating: '4',
     quantity: '5',
-    imageURL: 'https://www.newszii.com/wp-content/uploads/2017/postimg/abdul-kalam-hd-images06450742.jpg',
+    imageURL: 'https://www.newszii.com/wp-content/uploads/2018/07/Wings-of-Fire-An-Autobiography-of-APJ-Abdul-Kalam2.jpg',
   },
   {
     title: 'Dust Child Book',
     price: '5.00',
     rating: '4',
     quantity: '5',
-    image: 'src/images/dust-child-book.png',
+    image: 'https://shereads.com/wp-content/uploads/2022/11/Dust-Child-768x1159.jpg',
   },
   {
     title: 'The Wishing Game',
     price: '6.00',
     rating: '4',
     quantity: '5',
-    image: 'src/images/the-wishing-game-book.png',
+    image: 'https://checkreads.com/wp-content/uploads/2023/01/The-Wishing-Game.jpg',
   },
   {
     title: 'Same-Time-Next-Summer',
     price: '7.00',
     rating: '4',
     quantity: '5',
-    image: 'src/images/same-time-next-summer-book.png',
+    image: 'https://target.scene7.com/is/image/Target/GUEST_dd494d18-603e-4701-9419-912ac3bb475d?wid=488&hei=488&fmt=pjpeg',
   },
   {
     title: 'Homecoming Book',
     price: '8.00',
     rating: '4',
     quantity: '5',
-    image: 'src/images/homecoming-book.png',
+    image: 'https://imgv2-2-f.scribdassets.com/img/word_document/224261807/original/36699ca4a0/1583284519?v=1',
   },
   {
-    title: 'Gone Tonight Book',
+    title: 'Symphony of Secrets',
     price: '9.00',
     rating: '4',
     quantity: '5',
-    image: book25,
+    image: 'https://barbarah.wordpress.com/files/2008/02/symphony-of-secrets.jpg',
   }]
 };
 
